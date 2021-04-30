20df10dbd32e5da9c83f7394da30aaeb0f182271
Nicorvenius <nicorvenius@gmail.com>
2021-04-30 17:00:20 +0300

    Fix static directory for deploy

--- gusto/settings.py
+++ gusto/settings.py
@@ -25,7 +25,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../static'),
 )
 
 # Quick-start development settings - unsuitable for production
