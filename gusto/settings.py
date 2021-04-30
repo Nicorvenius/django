10203d97879fa742f2593a40267221ad7b50876b
Nicorvenius <nicorvenius@gmail.com>
2021-04-30 16:41:02 +0300

    Add new setting for deploy

--- gusto/settings.py
+++ gusto/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
@@ -21,8 +21,7 @@ DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 MEDIA_URL = '/media/'
@@ -40,9 +39,10 @@ STATICFILES_DIRS = (
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '2mtwqe#*!4afhw6_1yzh(_0u9d-k_a$#l_$2h$k5@w^%+_l'
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = SECRET_KEY
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['delicious-gusto.herokuapp.com']
 
 
 # Application definition
@@ -97,12 +97,28 @@ WSGI_APPLICATION = 'gusto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': '5432',
+    }
+}
 
 import dj_database_url
 db = dj_database_url.config()
